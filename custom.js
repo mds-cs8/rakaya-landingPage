eceef0d7685ccddf6136d663b9a7cfb3e1181860
Ahmed Salem <Meed.ssss8@gmail.com>
2023-07-13 15:38:24 +0300

    complete all requirements

---
+++ custom.js
@@ -0,0 +1,9 @@
+// this function to loaded intro when visit website ---
+let intro = document.getElementById("intro");
+
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    intro.style.top = "-100vh";
+  }, 2000);
+});
+// -----------------------------------------------------
