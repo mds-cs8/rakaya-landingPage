8e635757ff3566b70dc7dacbe2c3b8938baf9b98
Ahmed Salem <Meed.ssss8@gmail.com>
2023-07-13 16:53:25 +0300

    complete all requirements

--- custom.js
+++ custom.js
@@ -7,24 +7,20 @@ window.addEventListener("DOMContentLoaded", () => {
   }, 2000);
 });
 // -----------------------------------------------------
-const show = document.getElementsByClassName("counter"),
-  number = Number(show.innerHTML) + 1;
+function countUp(numberElement) {
+  let current = 0;
+  const total = parseInt(numberElement.textContent);
+  const increment = Math.ceil(total / 1000);
 
-let counter = 0,
-  delay = 1,
-  x = number / 2,
-  y = 0;
-
-counter_js();
-
-function counter_js() {
-  y++;
-  delay = x - y;
-  show.innerHTML = counter.toString();
-  counter++;
-  if (counter < number) {
-    setTimeout(function () {
-      counter_js();
-    }, 2000);
+  function step() {
+    current += increment;
+    if (current > total) current = total;
+    numberElement.textContent = current.toLocaleString();
+    current !== total && requestAnimationFrame(step);
   }
+  setTimeout(() => {
+    step();
+  }, 2000);
 }
+
+document.querySelectorAll(".counter").forEach((elem) => countUp(elem));
