3da765a126ae4b2baa10a082f36e8467302c04f5
Ahmed Salem <Meed.ssss8@gmail.com>
2023-07-13 16:25:31 +0300

    complete all requirements

--- custom.js
+++ custom.js
@@ -7,3 +7,24 @@ window.addEventListener("DOMContentLoaded", () => {
   }, 2000);
 });
 // -----------------------------------------------------
+const show = document.getElementsByClassName("counter"),
+  number = Number(show.innerHTML) + 1;
+
+let counter = 0,
+  delay = 1,
+  x = number / 2,
+  y = 0;
+
+counter_js();
+
+function counter_js() {
+  y++;
+  delay = x - y;
+  show.innerHTML = counter.toString();
+  counter++;
+  if (counter < number) {
+    setTimeout(function () {
+      counter_js();
+    }, 2000);
+  }
+}
