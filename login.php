e2fdb121389d60de4df9aea652577a0ccb3620e6
Ahmed Salem <Meed.ssss8@gmail.com>
2023-07-20 03:31:58 +0300

    add backend for sign up and login

--- login.php
+++ login.php
@@ -1,3 +1,73 @@
+<?php
+session_start();
+if(isset($_SESSION['user'])){
+    header('location:index.php');
+    exit();
+}
+if(isset($_COOKIE['email']) && isset($_COOKIE['password'])){
+    $mail = $_COOKIE['email']; 
+    $pass = $_COOKIE['password']; 
+}else{
+    $mail = "";
+    $pass = "";
+}
+
+if(isset($_POST['submit'])){
+ include 'conn-db.php';
+   $password=filter_var($_POST['password'],FILTER_SANITIZE_STRING);
+   $email=filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
+
+   if(isset($_POST['remember'])){
+    setcookie("email" , $_POST['email'], time() + (60 * 60 * 24));
+    setcookie("password" , $_POST['password'], time() + (60 * 60 * 24));
+   }
+   $errors=[];
+   
+
+   // validate email
+   if(empty($email)){
+    $errors[]="يجب كتابة البريد الاكترونى";
+   }
+
+
+   // validate password
+   if(empty($password)){
+        $errors[]="يجب كتابة  كلمة المرور ";
+   }
+
+
+
+   // insert or errros 
+   if(empty($errors)){
+   
+
+    $stm="SELECT * FROM user WHERE email ='$email'";
+    $q=$conn->prepare($stm);
+    $q->execute();
+    $data=$q->fetch();
+    if(!$data){
+       $errors[] = "لا يوجد حساب بهذا الايميل";
+    }else{
+        
+         $password_hash=$data['password']; 
+         
+         if(!password_verify($password,$password_hash)){
+            $errors[] = "خطأ فى تسجيل الدخول";
+         }else{
+            $_SESSION['user']=[
+                "name"=>$data['name'],
+                "email"=>$email,
+              ];
+            header('location:index.php');
+
+         }
+    }
+     
+    
+   }
+}
+
+?>
 <!DOCTYPE html>
 
 
@@ -9,13 +79,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rakaya</title>
 
-         <!-- ui library >> tailwend -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet" >
-       <!-- google fonts -->
+    <!-- ui library >> tailwend -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet">
+    <!-- google fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap" rel="stylesheet">
-        <!-- animation library -->
+    <!-- animation library -->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-        <!-- custtom css -->
+    <!-- custtom css -->
     <link rel="stylesheet" href="login.css">
 </head>
 
@@ -23,7 +93,7 @@
 <body>
 
     <section>
-        
+
         <div class="loginBox1">
 
             <div data-aos="fade-up" data-aos-duration="1000" class="loginBox2">
@@ -39,34 +109,42 @@
 
                 </div>
 
-                 <!-- log in form -->
+                <!-- log in form -->
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1
                         class="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         تسجيل الدخول
                     </h1>
 
-                <form class="space-y-4 md:space-y-6" action="#" method="POST">
-                     
+                    <form class="space-y-4 md:space-y-6" action="login.php" method="POST">
+                    <?php 
+        if(isset($errors)){
+            if(!empty($errors)){
+                foreach($errors as $msg){
+                    echo $msg . "<br>";
+                }
+            }
+        }
+    ?>
                         <div>
                             <label for="email"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">الايميل</label>
-                            <input type="email" name="email" id="email" class="inputBoxs" placeholder="name@google.com"
-                                required="">
+                            <input type="email" name="email" placeholder="name@google.com" id="email" class="inputBoxs" value="<?php echo $mail?> "
+                                required>
                         </div>
                         <div>
                             <label for="password"
                                 class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">كلمة المرور</label>
                             <input type="password" name="password" id="password" placeholder="••••••••"
-                                class="inputBoxs " required="">
+                                class="inputBoxs " required value="<?php echo $pass?>">
                         </div>
                         <div class="flex items-center justify-between  ">
 
                             <div class="flex items-start ">
 
                                 <div class="flex items-center h-5 checked:border-gray-900 , changecheckbox  ">
-                                    <input id="remember" aria-describedby="remember" type="checkbox" class=""
-                                        required="">
+                                    <input id="remember" name="remember" aria-describedby="remember" type="checkbox" class=""
+                                        >
                                 </div>
 
                                 <div class="mx-1 text-sm">
@@ -78,11 +156,11 @@
                                 class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">نسيت
                                 كلمة المرور</a>
                         </div>
-                        <button type="submit"
+                        <button type="submit" name="submit"
                             class="w-full h-12 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center border border-gray-300">تسجيل
                             الدخول</button>
                         <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            لم تسجل بعد ؟ <a href="signUp.html"
+                            لم تسجل بعد ؟ <a href="signUp.php"
                                 class="font-medium text-gray-900 hover:underline dark:text-primary-500">سجل الآن</a>
                         </p>
                     </form>
@@ -92,14 +170,14 @@
 
     </section>
 
-     
+
     <!-- flowbite script -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
 
     <!-- javascript library link -->
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
-     <!-- initialize AOS library script -->
+    <!-- initialize AOS library script -->
     <script>
         AOS.init();
     </script>
---
+++ signUp.php
@@ -0,0 +1,297 @@
+<!-- <?php
+session_start();
+if(isset($_SESSION['user'])){
+    header('location:index.php');
+    exit();
+}
+if(isset($_POST['submit'])){
+include 'conn-db.php';
+   $name=filter_var($_POST['name1'].$_POST['name2'],FILTER_SANITIZE_STRING);
+   $password=filter_var($_POST['password'],FILTER_SANITIZE_STRING);
+   $repassword=filter_var($_POST['repassword'],FILTER_SANITIZE_STRING);
+   $email=filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
+   $phone=filter_var($_POST['phone'],FILTER_SANITIZE_NUMBER_INT);
+   $gender=filter_var($_POST['gender'],FILTER_SANITIZE_STRING);
+   $users=filter_var($_POST['users'],FILTER_SANITIZE_STRING);
+
+   $errors=[];
+   // validate name
+   if(empty($name)){
+       $errors[]="يجب كتابة الاسم";
+   }elseif(strlen($name)>100){
+       $errors[]="يجب ان لايكون الاسم اكبر من 100 حرف ";
+   }
+
+   // validate email
+   if(empty($email)){
+    $errors[]="يجب كتابة البريد الاكترونى";
+   }elseif(filter_var($email,FILTER_VALIDATE_EMAIL)==false){
+    $errors[]="البريد الاكترونى غير صالح";
+   }
+
+   $stm="SELECT email FROM user WHERE email ='$email'";
+   $q=$conn->prepare($stm);
+   $q->execute();
+   $data=$q->fetch();
+
+   if($data){
+     $errors[]="البريد الاكترونى موجود بالفعل";
+   }
+
+
+   // validate password
+   if(empty($password)){
+        $errors[]="يجب كتابة  كلمة المرور ";
+   }elseif(strlen($password)<6){
+    $errors[]="يجب ان لايكون كلمة المرور  اقل  من 6 حرف ";
+}
+
+
+
+   // insert or errros 
+   if(empty($errors)){
+      echo "insert db";
+      $password=password_hash($password,PASSWORD_DEFAULT);
+      $stm="INSERT INTO user (name,phoneNumper,email,password , userType ,gender) VALUES ('$name','$phone','$email','$password','$users' , '$gender')";
+      $conn->prepare($stm)->execute();
+      $_POST['name']='';
+      $_POST['email']='';
+      $_POST['password']='';
+      $_POST['phone']='';
+
+      $_SESSION['user']=[
+        "name"=>$name,
+        "email"=>$email,
+      ];
+      header('location:index.php');
+   }
+}
+
+?> -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rakaya</title>
+
+    <!-- ui library >> tailwend -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.css" rel="stylesheet">
+    <!-- google fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap" rel="stylesheet">
+    <!-- animation library -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="signUp.css">
+
+</head>
+
+
+<body>
+
+    <!-- 1section - right side -->
+    <section class="signupSectionClass">
+
+        <div class="signupBox1">
+
+
+            <div data-aos="fade-up" data-aos-duration="1000" class="signupBox2 ">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1
+                        class="text-center text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                        التسجيل
+                    </h1>
+
+                    <form class="space-y-4 md:space-y-6 ,form" action="signUp.php" method="POST">
+                        <!-- name -->
+                        <div class="name">
+
+                            <div class="name1">
+
+                                <label for="name1" class="mb-4 text-sm font-medium  text-blue-900 dark:text-white">الاسم
+                                    الأول</label>
+
+                                <input type="text" name="name1" id="name1"
+                                    class="bg-gray-50  text-gray-900 sm:text-sm rounded-md block w-full p-2.5 dark:placeholder-gray-400 dark:text-white inputBoxs"
+                                    placeholder="سارة" required>
+                                <small id="name1_msg"></small>
+
+
+                            </div>
+
+                            <div class="name2">
+
+                                <label for="name2" class=" mb-2 text-sm font-medium text-gray-900 dark:text-white">الاسم
+                                    الأخير</label>
+                                <input type="text" name="name2" id="name2"
+                                    class="bg-gray-50  text-gray-900 sm:text-sm rounded-md block w-full p-2.5 dark:placeholder-gray-400 dark:text-white inputBoxs"
+                                    placeholder="محمد" required>
+                                <small id="name2_msg"></small>
+
+                            </div>
+
+                        </div>
+
+                        <!-- email -->
+                        <div class="email-phone">
+                            <div class="email">
+                                <label for="email"
+                                    class=" mb-2 text-sm font-medium text-gray-900 dark:text-white">الايميل</label>
+                                <input type="email" name="email" id="email"
+                                    class="bg-gray-50  text-gray-900 sm:text-sm rounded-md block w-full p-2.5 dark:placeholder-gray-400 dark:text-white inputBoxs"
+                                    placeholder="name@google.com" required>
+                                <small id="email_msg"></small>
+                            </div>
+
+                            <!-- phone number -->
+                            <div class="phone">
+                                <label for="phone" class=" mb-2 text-sm font-medium text-gray-900 dark:text-white">رقم
+                                    الهاتف</label>
+                                <input type="tel" id="phone" name="phone"
+                                    class="bg-gray-50  text-gray-900 sm:text-sm rounded-md block w-full p-2.5 dark:placeholder-gray-400 dark:text-white inputBoxs"
+                                    required>
+                                <small id="phone_msg"></small>
+
+                            </div>
+                        </div>
+
+                        <!-- password -->
+                        <div class="password">
+
+                            <!-- pass1 -->
+                            <div class="pass1">
+                                <label for="password"
+                                    class=" mb-2 text-sm font-medium text-gray-900 dark:text-white">كلمة
+                                    المرور</label>
+                                <input type="password" name="password" id="password" placeholder="••••••••"
+                                    class="bg-gray-50  text-gray-900 sm:text-sm rounded-md block w-full p-2.5 dark:placeholder-gray-400 dark:text-white inputBoxs"
+                                    required>
+
+                                <small id="password_msg">
+                                    كلمة المرور يجب أن <strong>لا تقل عن 6 أرقام</strong> ( 1 حرف صغير ,1 حرف كبير, رمز
+                                    وأرقام)
+
+                                </small>
+
+                            </div>
+
+                            <div class="pass2">
+                                <!-- pass2 -->
+                                <label for="password2" class=" mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                    تأكيد كلمة المرور
+                                </label>
+                                <input type="password" name="repassword" id="repassword" placeholder="••••••••"
+                                    class="bg-gray-50  text-gray-900 sm:text-sm rounded-md block w-full p-2.5 dark:placeholder-gray-400 dark:text-white inputBoxs"
+                                    required>
+                                <small id="repassword_msg">
+
+                                </small>
+                            </div>
+
+
+
+                        </div>
+
+
+
+
+
+
+                        <!-- gender & userType -->
+                        <div class="genderAndUserType">
+
+                            <!-- gender -->
+                            <div class="inline-flex flex gender ">
+
+                                <label for="gender"
+                                    class=" ml-3 mb-2 text-sm font-medium text-gray-900 dark:text-white">الجنس :</label>
+
+                                <input type="radio" id="female" name="gender" value="female"
+                                    class="ml-2 border border-gray-300" required>
+                                <label for="female">أنثى</label>
+
+                                <input type="radio" id="male" name="gender" value="male"
+                                    class="mr-3   ml-2 bg-gray-50 border border-gray-300" required>
+                                <label for="male">ذكر</label>
+
+                            </div>
+
+
+                            <!-- user type -->
+                            <div class=" inline-flex flex userType">
+
+                                <label for="users" class=" ml-3 mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                    حدد الفئة :</label>
+
+                                <select name="users" id="users" size="1"
+                                    class=" py-px  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-md focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 , users">
+
+                                    <option value="Developer">مطور</option>
+                                    <option value="Consultant"> الاستشارات</option>
+                                    <option value="Clint">عميل</option>
+                                    <option value="Intern">متدرب</option>
+                                </select>
+
+                            </div>
+
+                        </div>
+
+
+
+                        <!-- submit -->
+                        <button type="submit" id="sign-btn" name="submit"
+                            class="w-full h-12 text-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 text-center">تسجيل</button>
+                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                            لديك حساب بالفعل ؟<a href="login.html"
+                                class="font-medium text-gray-900 hover:underline dark:text-primary-500 , refrencelogin">
+                                تسجيل الدخول </a>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- 2section -left side -->
+    <section class="headOfSingsection ">
+
+        <div data-aos="fade-up" data-aos-duration="1000" class="headOfSign">
+
+            <a href="index.html">
+
+                <img src="./assets/ركايا_full_.png" alt="rakaya logo">
+            </a>
+
+            <h1> ركايا آفاق واسعة وامكانيات عالية</h1>
+
+            <h2> انضم معنا !</h2>
+
+
+        </div>
+
+
+
+    </section>
+
+
+
+    <!-- flowbite script -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.7.0/flowbite.min.js"></script>
+
+    <!-- javascript library link -->
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <!-- initialize AOS library script -->
+    <script>
+        AOS.init();
+    </script>
+
+    <script src="./custom.js"></script>
+
+</body>
+
+</html>
