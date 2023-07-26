d26511144655437561d29c3f3f2974eb61aa6c1d
esra <esro@gmail.com>
2023-07-26 15:10:51 +0300

    profile

--- signUp.php
+++ signUp.php
@@ -263,8 +263,6 @@ include 'conn-db.php';
                         </div>
                         <div class="img ">
 
-
-
                                 <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                                     for="file_input">رفع صورة</label>
                                     <input class="w-full h-10 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file" name="UserImg">
